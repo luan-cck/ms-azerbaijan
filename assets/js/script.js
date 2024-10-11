499c21b29d83ec856f82d54b5a3a65e2666b36e1
Kota-704 <kktt14590358@gmail.com>
2024-10-11 17:42:34 +0900

    ♻️ /一部ページのリファクタリング

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+
+/*--------------------
+set magnifier
+--------------------*/
+const mapContainer = document.querySelector('.map_image');
+const magnifierIcons = document.querySelectorAll('.magnifier_icon');
+
+function setIconPositions() {
+    const contiainerWidth = mapContainer.offsetWidth;
+    const containerHeight = mapContainer.offsetHeight;
+
+    magnifierIcons.forEach(icon => {
+        const relativeX = parseFloat(icon.getAttribute('data-x'));
+        const relativeY = parseFloat(icon.getAttribute('data-y'));
+
+        icon.style.left = `${relativeX * contiainerWidth}px`;
+        icon.style.top = `${relativeY * containerHeight}px`;
+    })
+}
+
+window.addEventListener('resize', setIconPositions);
+document.addEventListener('DOMContentLoaded', setIconPositions);
+
+
+/*--------------------
+switch language
+--------------------*/
+
 document.getElementById('language-form').addEventListener('change', function (event) {
     if (event.target.value === 'japanese') {
         window.location.href = './index.html';
