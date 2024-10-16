a085fe4acf7cdee0d390ca189b7dfa3b8ccf850f
Kota-704 <kktt14590358@gmail.com>
2024-10-16 18:47:20 +0900

    🚧 /デザイン変更に伴い、コードの編集中

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,3 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const windowWidth = window.innerWidth;
+  const space = Math.max((windowWidth - 1250) / 2, 0);
+
+  const consortiumSwiper = new Swiper(".consortium_swiper", {
+    centeredSlides: false,
+    slidesPerView: 1,
+    spaceBetween: space,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      init: function () {
+        const swiperWrapper = document.querySelector(
+          ".consortium_swiper .swiper-wrapper"
+        );
+        if (swiperWrapper) {
+          swiperWrapper.style.transform = "translate3d(0, 0, 0)";
+        }
+      },
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const firstSlide = document.querySelector(
+    ".consortium_swiper .swiper-slide:first-child"
+  );
+
+  if (firstSlide) {
+    firstSlide.style.marginLeft = "0px";
+  }
+});
+
+consortiumSwiper.on("slideChange", function () {
+  const firstSlide = document.querySelector(
+    ".consortium_swiper .swiper-slide:first-child"
+  );
+
+  if (firstSlide) {
+    firstSlide.style.marginLeft = "0px";
+  }
+});
+
 const swiper = new Swiper(".swiper", {
   centeredSlides: false,
   slidesPerView: 2.7,
