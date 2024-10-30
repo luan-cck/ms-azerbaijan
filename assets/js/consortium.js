dd3471911c2b9262cb789ab58577efc5faddf45a
Kota-704 <kktt14590358@gmail.com>
2024-10-30 18:34:14 +0900

    ♻️ /一部コードを移植

--- assets/js/consortium.js
+++ assets/js/consortium.js
@@ -16,3 +16,52 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+/*--------------------
+consortium_swiper
+--------------------*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const consortiumSwiperElement = document.querySelector(".consortium_swiper");
+  if (consortiumSwiperElement) {
+    const windowWidth = window.innerWidth;
+    const space = Math.max((windowWidth - 1250) / 2, 0);
+
+    // Globalize for calling in consortium.js
+    window.consortiumSwiper = new Swiper(".consortium_swiper", {
+      centeredSlides: false,
+      slidesPerView: 1,
+      spaceBetween: space,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      on: {
+        init: function () {
+          const swiperWrapper = document.querySelector(
+            ".consortium_swiper .swiper-wrapper"
+          );
+          if (swiperWrapper) {
+            swiperWrapper.style.transform = "translate3d(0, 0, 0)";
+          }
+        },
+      },
+    });
+
+    consortiumSwiper.on("slideChange", function () {
+      const firstSlide = document.querySelector(
+        ".consortium_swiper .swiper-slide:first-child"
+      );
+      if (firstSlide) {
+        firstSlide.style.marginLeft = "0px";
+      }
+    });
+
+    const firstSlide = document.querySelector(
+      ".consortium_swiper .swiper-slide:first-child"
+    );
+    if (firstSlide) {
+      firstSlide.style.marginLeft = "0px";
+    }
+  }
+});
